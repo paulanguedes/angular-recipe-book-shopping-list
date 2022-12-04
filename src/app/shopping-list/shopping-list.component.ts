1c156a047399eef15ab17f5f5bea92c72c6dfea0
paulanguedes <paulanguedes@gmail.com>
2022-12-04 15:32:50 -0300

    :card_file_box: create recipe's model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,11 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
