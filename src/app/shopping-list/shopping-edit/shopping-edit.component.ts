8759d6661ab585fe6b506c84f800ef68f8fdfa6b
paulanguedes <paulanguedes@gmail.com>
2023-01-06 18:29:26 -0300

    :art: add ingridients function ViewChild

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../../shared/ingredient.model';
+import { Component, ElementRef, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+
+  @Output() ingridientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const ingridientName = this.nameInputRef.nativeElement.value;
+    const ingridientAmount = this.amountInputRef.nativeElement.value;
+    const newIngridient = new Ingredient(ingridientName, ingridientAmount);
+
+    this.ingridientAdded.emit(newIngridient);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  onIngridientAdded(ingridient: Ingredient) {
+    this.ingredients.push(ingridient);
+  }
 }
