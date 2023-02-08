2ac42d5e975adcf2c40d5eac17ab2aa1c2ebf013
paulanguedes <paulanguedes@gmail.com>
2023-02-08 17:29:55 -0300

    :art: create save data function of header comp

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { DataStorageService } from './../shared/data-storage.service';
 import { Component } from "@angular/core";
 
 @Component({
@@ -5,5 +6,11 @@ import { Component } from "@angular/core";
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+  constructor(
+    private dataStorageService: DataStorageService
+  ) {}
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from './../recipes/recipe.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private recipeService: RecipeService
   ) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(
+      'https://angular-recipe-book-9c6f3-default-rtdb.firebaseio.com/recipes.json',
+      recipes
+    ).subscribe(response => {
+      console.log(response);
+
+    });
+  }
 }
