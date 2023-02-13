ce96e11a8a093fbb9650d44735cd717f8919a2c8
paulanguedes <paulanguedes@gmail.com>
2023-02-13 17:33:09 -0300

    :art: send signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,7 +10,9 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
   isLoginMode = true;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +22,25 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form. value.password;
+
+    if (this.isLoginMode) {
+      //...
+    } else {
+      this.authService.signup(email, password)
+        .subscribe(responseData => {
+          console.log(responseData);
+        }, error => {
+          console.log(error);
+        }
+      );
+    }
+
     form.reset();
   }
 
