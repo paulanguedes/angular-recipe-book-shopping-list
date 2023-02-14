aab7d24e94d0c1df5fb98406881c83efae080483
paulanguedes <paulanguedes@gmail.com>
2023-02-14 14:01:02 -0300

    :art: handle error

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
   idToken: string,
@@ -26,7 +28,25 @@ signup(email: string, password: string) {
       password: password,
       returnSecureToken: true
     }
-  )
+  ).pipe(catchError(errorResponse => {
+    let errorMessage = 'An unknown error occured!';
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Email already exists!';
+        break;
+
+      default:
+        break;
+    }
+
+    return throwError(errorMessage);
+
+  }));
 }
 
 }
