e2eeb36f3e9fefdd7374ac09a070a65a0e682790
paulanguedes <paulanguedes@gmail.com>
2023-02-22 13:04:36 -0300

    :art: create and config core module

---
+++ src/app/core.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ]
+})
+export class CoreModule { }
