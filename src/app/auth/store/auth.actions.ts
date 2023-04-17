dc23a77a388f7c5e2f545951808dfc9e1849daf0
paulanguedes <paulanguedes@gmail.com>
2023-04-17 16:49:46 -0300

    :bug: fix dependency conflicts

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store';
 
 export const LOGIN_START = '[Auth] Login Start';
-export const LOGIN_FAIL = '[Auth] Login Fail'
+export const LOGIN_FAIL = '[Auth] Login Fail';
 export const LOGIN = '[Auth] Login';
 export const LOGOUT = '[Auth] Logout';
 
