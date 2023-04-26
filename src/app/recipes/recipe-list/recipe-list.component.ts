ea444813daa63f454fbf1a4f4cf372ad88ab891c
paulanguedes <paulanguedes@gmail.com>
2023-04-26 20:22:30 -0300

    :construction: add Set Recipes action

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
-import { RecipeService } from './../recipe.service';
+// import { RecipeService } from './../recipe.service';
 import { Recipe } from '../recipe.model';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-list',
@@ -15,17 +18,24 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   subscription: Subscription;
 
   constructor(
-    private recipeService: RecipeService,
+    // private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>
   ) { }
 
   ngOnInit(): void {
-    this.subscription = this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
-      this.recipes = recipes;
+    // this.subscription = this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+    //   this.recipes = recipes;
+    // })
+    this.subscription = this.store
+      .select('recipes')
+      .pipe(map(recipesState => recipesState.recipes))
+      .subscribe((recipes: Recipe[]) => {
+        this.recipes = recipes;
     })
 
-    this.recipes = this.recipeService.getRecipes();
+    // this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { RecipeService } from './../recipes/recipe.service';
-import { Recipe } from "../recipes/recipe.model";
-import { AuthService } from "../auth/auth.service";
+import { Recipe } from   '../recipes/recipe.model';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+// import { AuthService } from "../auth/auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +16,8 @@ export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService
+    private store: Store<fromApp.AppState>
+    // private authService: AuthService
   ) {}
 
   storeRecipes() {
@@ -38,7 +42,8 @@ export class DataStorageService {
         });
       }),
       tap(recipes => {
-        this.recipeService.setRecipes(recipes);
+        // this.recipeService.setRecipes(recipes);
+        this.store.dispatch(new RecipesActions.SetRecipes(recipes));
       })
     )
   }
